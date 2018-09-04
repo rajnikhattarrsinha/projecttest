42aae35b1b08f0f01383682d28bd9eac7810126f
Pravesh Jha <pravesh28@gmail.com>
2018-09-04 19:34:32 +0530

    Update
    Update

--- src/test/java/net/enablers/tvstack/steps/ChannelSetupPageSteps.java
+++ src/test/java/net/enablers/tvstack/steps/ChannelSetupPageSteps.java
@@ -278,24 +278,28 @@ public class ChannelSetupPageSteps {
     @Then("^I should be able to enter value '(.*)' in GRPs Celebrated textbox$")
     public void iWillEnterGRPsCelebrated(String GRPs ) throws Throwable
     {
+        channelSetupPage.entervalueinTextbox("GRPs Calibrated at","");
         channelSetupPage.entervalueinTextbox("GRPs Calibrated at",GRPs);
         appliEyes.capture("GRPs Celebrated value '"+GRPs+"' enter successfully.");
     }
     @Then("^I should be able to enter value '(.*)' in Reach populated textbox")
     public void iWillEnterinReachTextbox(String Reach ) throws Throwable
     {
+        channelSetupPage.entervalueinTextbox("Reach","" );
         channelSetupPage.entervalueinTextbox("Reach",Reach );
         appliEyes.capture("Reach value '"+Reach+"' enter successfully.");
     }
     @Then("^I should be able to enter value '(.*)' in Maximum Reach textbox")
     public void iWillEnterinMaximumReachtextbox(String MaximumReach) throws Throwable
     {
+        channelSetupPage.entervalueinTextbox("Maximum Reach","" );
         channelSetupPage.entervalueinTextbox("Maximum Reach",MaximumReach );
         appliEyes.capture("Maximum Reach value '"+MaximumReach+"' enter successfully.");
     }
     @Then("^I should be able to enter value '(.*)' in Precision textbox")
     public void iWillEnterinPrecisiontextbox(String Precisionvalue ) throws Throwable
     {
+        channelSetupPage.entervalueinTextbox("Precision","" );
         channelSetupPage.entervalueinTextbox("Precision",Precisionvalue );
         appliEyes.capture("Precision value '"+Precisionvalue+"' enter successfully.");
     }
